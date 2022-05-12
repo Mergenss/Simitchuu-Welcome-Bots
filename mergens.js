79d03b932121484d97c6b4ff69431d4a27a8a8c4
Mergens Flex <93868137+Mergenss@users.noreply.github.com>
2022-05-12 18:33:30 +0300

    Add files via upload

---
+++ mergens.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const database = require("quick.db")
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+    client.channels.cache.get("973249213678895137").join();
+});
+
+client.on("ready", () => {
+    client.user.setActivity("Mergenss  ❤️ Phoenix")
+console.log("Bot Başarı İle Sese Girdi")
+});
+
+
+
+client.login("OTczNjAxNzYyOTY5ODA0ODUx.G1V6zV.JULPgsLcYowI4VKAXvcHmTBwPrCbNjG2ZWkuKo")
